6ebfbd7b2ff9cb2fa109460ffe5f654c6fc7f303
Sunil Dias Azari <sunildias.azari@prudential.com>
2017-09-04 19:45:14 -0400

    Adding Right Structure

--- Community/Xfers/Model/Xfers.php
+++ Community/Xfers/Model/Xfers.php
@@ -27,7 +27,36 @@ class Xfers extends \Magento\Payment\Model\Method\AbstractMethod
      */
     protected $_isOffline = true;
 
-
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Magento\Directory\Model\CountryFactory $countryFactory,
+        array $data = array()
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $moduleList,
+            $localeDate,
+            null,
+            null,
+            $data
+        );
+        
+        echo $this->getConfigData('api_key');exit;
+    }
   
 
 }
