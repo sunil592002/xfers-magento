bbfb4e4c6a288e06d92f7fbdcd6e3b21135bd32d
Sunil Dias Azari <sunildias.azari@prudential.com>
2017-09-04 07:40:59 -0400

    cahnbing folder structure

--- app/code/community/Xfers/registration.php
+++ app/code/community/Xfers/registration.php
@@ -1,6 +1,6 @@
 <?php
   \Magento\Framework\Component\ComponentRegistrar::register(
       \Magento\Framework\Component\ComponentRegistrar::MODULE,
-      'Xfers',
+      'community_Xfers',
       __DIR__
   );
