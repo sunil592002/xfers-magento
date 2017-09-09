17be16e245f3f0e0a20f755b89e62ce718e76ad1
Sunil Dias Azari <sunildias.azari@prudential.com>
2017-09-09 19:36:51 -0400

    Add telephone number

--- Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
+++ Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
@@ -1,34 +1,27 @@
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 /*browser:true*/
 /*global define*/
 define(
     [
-        'uiComponent',
-        'Magento_Checkout/js/view/payment/default',
-        'Magento_Customer/js/model/customer'
+        'Magento_Checkout/js/view/payment/default'
     ],
-    function (Component, payment, customer) {
+    function (Component) {
         'use strict';
+
         return Component.extend({
             defaults: {
                 template: 'Community_Xfers/payment/xfers'
             },
-            isCustomerLoggedIn: customer.isLoggedIn,
 
-            /** Returns payment method instructions */
-            getInstructions: function() {
-                return window.checkoutConfig.payment.instructions[this.item.method];
+            /** Returns send check to info */
+            getMailingAddress: function() {
+                return window.checkoutConfig.payment.checkmo.mailingAddress;
             },
 
-            /**
-             * Get payment method code.
-             */
-            getCode: function () {
-                return this.item.method;
-            },
+           
         });
     }
 );
