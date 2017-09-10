fdab1ed09901f44d397072d45664be353728a7c4
Sunil Dias Azari <sunildias.azari@prudential.com>
2017-09-10 16:45:04 -0400

    Add telephone number debug

--- Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
+++ Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
@@ -7,7 +7,8 @@
 define(
     [
         'Magento_Checkout/js/view/payment/default',
-        //'Magento_Customer/js/model/customer'
+        'Magento_Customer/js/model/customer',
+
     ],
     function (Component, customer) {
         'use strict';
@@ -16,13 +17,20 @@ define(
             defaults: {
                 template: 'Community_Xfers/payment/xfers'
             },
-            //isCustomerLoggedIn: customer.isLoggedIn,
+            isCustomerLoggedIn: customer.isLoggedIn,
 
             /** Returns send check to info */
             getMailingAddress: function() {alert(window.checkoutConfig.payment.checkmo.mailingAddress);
                 alert(window.checkoutConfig.payment.checkmo.telephone);
                 return window.checkoutConfig.payment.checkmo.mailingAddress;
             },
+
+            /** Returns send check to info */
+            getTelephone: function() {alert(window.checkoutConfig.payment.checkmo.mailingAddress);
+                alert(window.checkoutConfig.payment.checkmo.telephone);
+                return window.checkoutConfig.payment.checkmo.mailingAddress;
+            },
+
 
            
         });
