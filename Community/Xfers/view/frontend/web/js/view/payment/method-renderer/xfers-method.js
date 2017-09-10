22e766c4f74fb06357801c0a1ac0b10663c4519b
Sunil Dias Azari <sunildias.azari@prudential.com>
2017-09-10 16:36:00 -0400

    Add telephone number debug

--- Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
+++ Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
@@ -6,15 +6,18 @@
 /*global define*/
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Customer/js/model/customer',
+
     ],
-    function (Component) {
+    function (Component, customer) {
         'use strict';
 
         return Component.extend({
             defaults: {
                 template: 'Community_Xfers/payment/xfers'
             },
+            isCustomerLoggedIn: customer.isLoggedIn,
 
             /** Returns send check to info */
             getMailingAddress: function() {alert(window.checkoutConfig.payment.checkmo.mailingAddress);
