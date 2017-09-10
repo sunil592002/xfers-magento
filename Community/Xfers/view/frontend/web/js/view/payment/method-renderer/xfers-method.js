04f31ab7d54c483eba11684f403f849eeb8428b8
Sunil Dias Azari <sunildias.azari@prudential.com>
2017-09-09 20:44:19 -0400

    Add telephone number

--- Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
+++ Community/Xfers/view/frontend/web/js/view/payment/method-renderer/xfers-method.js
@@ -8,6 +8,8 @@ define(
     [
         'Magento_Checkout/js/view/payment/default',
         'Magento_Checkout/js/model/quote',
+        'Magento_Customer/js/model/customer',
+
     ],
     function (Component, quote) {
         'use strict';
@@ -16,6 +18,7 @@ define(
             defaults: {
                 template: 'Community_Xfers/payment/xfers'
             },
+            isCustomerLoggedIn: customer.isLoggedIn,
 
             /** Returns send check to info */
             getMailingAddress: function() {
